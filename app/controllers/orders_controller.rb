e5eec305a2cbb3b5cd471e721f0a23ce321314f9
Aaron Grant <aarongeoffreygrant5@hotmail.com>
2017-04-19 09:06:16 -0700

    found that heroku crashed because I had an extra end in app/controlers/orders_controler.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,9 +37,7 @@ class OrdersController < ApplicationController
       end
     end
   end
-
-  end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
