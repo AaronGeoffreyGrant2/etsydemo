ba9e231c779f21f23d0541da10fb1d826d9d3672
Aaron Grant <aarongeoffreygrant5@hotmail.com>
2017-04-23 13:05:57 -0700

    Finaly got stripe connect to work

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -33,7 +33,23 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
 
+    if current_user.recipient.blank?
+
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Account.create(
+        :managed => false,
+        :country => 'US',
+        :email => current_user.email 
+        )
+    end
+
+    current_user.recipient = recipient.id
+    current_user.save
+
     respond_to do |format|
+      
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
         format.json { render :show, status: :created, location: @listing }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416192544) do
+ActiveRecord::Schema.define(version: 20170423172149) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170416192544) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "recipient"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
