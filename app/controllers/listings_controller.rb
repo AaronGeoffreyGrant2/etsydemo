9128d2d654f32a35097c74fd776505f5b9c3a785
Aaron Grant <aarongeoffreygrant5@hotmail.com>
2017-06-21 10:20:38 -0700

    fixed the recipient error in the listing controler by updating the stripe api to individual

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -34,27 +34,25 @@ class ListingsController < ApplicationController
     @listing.user_id = current_user.id
 
     if current_user.recipient.blank?
-
       Stripe.api_key = ENV["STRIPE_API_KEY"]
       token = params[:stripeToken]
 
-      recipient = Stripe::Account.create(
-        :managed => false,
-        :country => 'US',
-        :email => current_user.email 
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
         )
+
+      current_user.recipient = recipient.id
+      current_user.save
     end
 
-    current_user.recipient = recipient.id
-    current_user.save
-
     respond_to do |format|
-      
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -66,9 +64,9 @@ class ListingsController < ApplicationController
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -79,7 +77,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
