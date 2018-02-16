2d650b7a9bbe34acd59881c34e4de174161e7af1
AaronGeoffreyGrant2 <aarongeoffreygrant5@yahoo.com>
2018-02-16 08:19:27 -0800

    changed the text on the listing page and made it change when ever a user logged in or out

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,6 +274,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'email, user_posts, publish_actions'
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'], scope: 'email, user_posts, publish_actions'
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end
