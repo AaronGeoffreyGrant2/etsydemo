be00f8c99941eb2df914d4d290284021d2e3bd7d
AaronGeoffreyGrant2 <aarongeoffreygrant5@yahoo.com>
2018-02-18 17:12:10 -0800

    Solved the problem with not being able to signin with facebook offline by deleating all of the users that were not created by using the omniauth-facebook gem, am no going to test it on heroku.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,6 +274,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'], scope: 'email, user_posts, publish_actions'
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'],
+  callback_url: "http://localhost:3000/users/auth/facebook/callback",
+  scope: 'email, user_posts, publish_actions'
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end
