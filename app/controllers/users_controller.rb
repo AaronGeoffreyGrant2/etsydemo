cce7c41a27b4ed1553e32d4157043939b0b13728
AaronGeoffreyGrant2 <aarongeoffreygrant5@yahoo.com>
2018-07-15 16:42:00 -0700

    Added user profile pages and made the spaceshooter game more mobile compatable

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def index
+  	 @users = User.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  	@user_listings = @user.listings
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :listings do
     resources :orders, only: [:new, :create]
   end
+  resources :users
   resources :conversations do
     resources :messages
 
